3c3ec928bc2227ef4c082d73715742cb6221d1f2
Karl George <karlgeorge87@gmail.com>
2024-06-18 11:29:05 +0100

    Add Home screen

---
+++ app/home.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View className='w-full h-full bg-primary'>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
