b07a4067b56072c8bbd62674e84cef188adb3b67
Karl George <karlgeorge87@gmail.com>
2024-06-18 11:29:19 +0100

    Add redirect to home

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
+import { router } from 'expo-router';
 
 export default function Index() {
   return (
@@ -9,7 +10,10 @@ export default function Index() {
         <Text className='text-4xl leading-[48px] text-center text-white font-acme'>
           Find The Perfect Movies For You
         </Text>
-        <TouchableOpacity className='py-3 mt-10 rounded-md bg-accent'>
+        <TouchableOpacity
+          className='py-3 mt-10 rounded-md bg-accent'
+          onPress={() => router.push('/home')}
+        >
           <Text className='text-[28px] text-center text-white font-acme'>
             Get Started
           </Text>
