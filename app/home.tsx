f49d54c928bb601d6babf7b400c024a26218af7b
Karl George <karlgeorge87@gmail.com>
2024-06-18 12:57:54 +0100

    Style home page

--- app/home.tsx
+++ app/home.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 
 const Home = () => {
   return (
-    <View className='w-full h-full bg-primary'>
-      <Text>Home</Text>
+    <View className='w-full h-full px-4 pt-8 bg-primary'>
+      <View>
+        <Text className='text-2xl text-text font-acme'>Top Movies</Text>
+        <FlatList
+          data={[1, 2, 3]}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          horizontal
+        />
+      </View>
+      {/* TODO: Categories */}
+      <View>
+        <Text className='text-2xl text-text font-acme'>Latest Movies</Text>
+        <FlatList
+          data={[1, 2, 3]}
+          renderItem={({ item }) => <Text>{item}</Text>}
+        />
+      </View>
     </View>
   );
 };
