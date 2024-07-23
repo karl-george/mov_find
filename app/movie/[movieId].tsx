48fe9220dcd195ae4c801b0dd70d86034f1c7258
Karl George <karlgeorge87@gmail.com>
2024-07-23 12:50:57 +0100

    More styling

--- app/movie/[movieId].tsx
+++ app/movie/[movieId].tsx
@@ -1,7 +1,6 @@
-import { View, Text, Image, SafeAreaView } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import { details } from '@/constants/data';
-import { Movie } from '@/components/MovieCardLarge';
 
 const MovieId = () => {
   // 'https://api.themoviedb.org/3/movie/1022789?language=en-US'
@@ -15,16 +14,27 @@ const MovieId = () => {
         className='w-full h-[220px]'
         resizeMode='cover'
       />
-      <View className='h-full px-6 bg-primary'>
+      <View className='w-full h-full px-6 bg-primary'>
         <Text className='mt-5 text-3xl text-center text-text font-acme'>
           {movieDetails.title}
         </Text>
-        <View className='mt-5'></View>
         <Text className='mt-5 text-2xl text-center text-text font-acme'>
           {movieDetails.tagline}
         </Text>
         <Text className='mt-5 text-lg text-text font-acme'>
           {movieDetails.overview}
+        </Text>
+        <Text className='mt-5 text-lg text-text font-acme'>
+          Genres: {movieDetails.genres.map((genre) => genre.name).join(', ')}
+        </Text>
+        <Text className='mt-2 text-lg text-text font-acme'>
+          Runtime: {movieDetails.runtime}m
+        </Text>
+        <Text className='mt-2 text-lg text-text font-acme'>
+          Release Date: {movieDetails.release_date}
+        </Text>
+        <Text className='mt-2 text-lg text-text font-acme'>
+          Rating: {movieDetails.vote_average.toFixed(1)}
         </Text>
       </View>
     </View>
